f3029189af6c06992daa34e46e39518211ab0729
solve <141151895+internalvpn@users.noreply.github.com>
2024-01-05 16:32:38 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,33 +1,33 @@
 import os
+from colorama import init, Fore, Style
 
-print("""
-\x1b[38;2;155;20;134m╦╔═ ╔═╗ ╦═╗ ╔╦╗ ╔═╗
-\x1b[38;2;155;20;147m╠╩╗ ╠═╣ ╠╦╝ ║║║ ╠═╣
-\x1b[38;2;155;20;147m╩ ╩ ╩ ╩ ╩╚═ ╩ ╩ ╩ ╩\x1b[38;2;0;500;58m <~~@Setup
-""") 
 
-print("""[1] pip\n[2] pip3\nChoose which one to install with.""")
+init()
 
-c = input("~|~: ")
-if c == "1":
-    os.system("pip install cloudscraper")
-    os.system("pip install socks")
-    os.system("pip install pysocks")
-    os.system("pip install colorama")
-    os.system("pip install undetected_chromedriver")
-    os.system("pip install httpx")
+def print_colored_text(text, color):
+    
+    colored_text = f'{color}{text}{Style.RESET_ALL}'
 
-elif c == "2":
-    os.system("pip3 install cloudscraper")
-    os.system("pip3 install socks")
-    os.system("pip3 install pysocks")
-    os.system("pip3 install colorama")
-    os.system("pip3 install undetected_chromedriver")
-    os.system("pip3 install httpx")
-if os.name == "nt":
-    pass
-else:
-    os.system("wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb")
-    os.system("apt-get install ./google-chrome-stable_current_amd64.deb")
+   
+    print(colored_text)
 
-print("Done.")
+
+print_colored_text("╦╔╗╔╔═╗╔╦╗╔═╗╦  ╦  ╦╔╗╔╔═╗  ╦═╗╔═╗╔═╗ ╦ ╦╦╦═╗╔═╗╔╦╗╔═╗╔╗╔╔╦╗╔═╗", Fore.GREEN)
+print_colored_text("║║║║╚═╗ ║ ╠═╣║  ║  ║║║║║ ╦  ╠╦╝║╣ ║═╬╗║ ║║╠╦╝║╣ ║║║║╣ ║║║ ║ ╚═╗", Fore.GREEN)
+print_colored_text("╩╝╚╝╚═╝ ╩ ╩ ╩╩═╝╩═╝╩╝╚╝╚═╝  ╩╚═╚═╝╚═╝╚╚═╝╩╩╚═╚═╝╩ ╩╚═╝╝╚╝ ╩ ╚═╝", Fore.GREEN)
+                                                                                                                                                                                   
+
+# Requirements
+os.system("pip install cloudscraper")
+os.system("pip install socks")
+os.system("pip install pysocks")
+os.system("pip install colorama")
+os.system("pip install undetected_chromedriver")
+os.system("pip install httpx")
+# pip3
+os.system("pip3 install cloudscraper")
+os.system("pip3 install socks")
+os.system("pip3 install pysocks")
+os.system("pip3 install colorama")
+os.system("pip3 install undetected_chromedriver")
+os.system("pip3 install httpx")
